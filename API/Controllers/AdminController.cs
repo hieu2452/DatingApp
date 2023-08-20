6387c670bda4f9d5bad89397c5e534a1586f1aa6
hieu2452 <hieu245202@gmail.com>
2023-08-20 22:32:54 +0700

    ROLE EDIT

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -26,8 +26,33 @@ namespace API.Controllers
                             Username = u.UserName,
                             Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
                         }).ToListAsync();
-        
+
             return Ok(users);
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPut("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles)
+        {
+            if (string.IsNullOrEmpty(roles)) return BadRequest("select at least one role");
+
+            var selectedRoles = roles.Split(",").ToArray();
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null) return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded) return BadRequest("failed to remove from role");
+
+            return Ok(await _userManager.GetRolesAsync(user));
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
